b478431eb77a2ab4f3b09441557065a80bc267bf
Evan J Luputra <eluputra@hotmail.com>
2019-07-12 21:01:57 -0700

    Submision of dysktra 4
    first submision have already play around and test that it still works

---
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,24 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+// this code is for setting up data base
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) // this code for building the model 
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
